500fed4b3e3968a15e9521cb0408e56d6aef8dbe
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-02-23 08:48:31 +0000

    Add LevelSelect

--- scripts/Common.esm.js
+++ scripts/Common.esm.js
@@ -2,23 +2,22 @@ export const HIDDEN_CLASS = 'hidden';
 export const HIDDEN_SCREEN = false;
 export const VISIBLE_SCREEN = true;
 
-
 export class Common {
     constructor(elementId) {
         this.elementId = this.bindToElement(elementId);
+        this.element = this.bindToElement(elementId);
     }
 
     bindToElement(elementToFindById) {
         const element = document.getElementById(elementToFindById);
-
         if (!element) {
             throw new Error(`Nie znaleziono elementu o id: ${elementToFindById}`);
         }
-
         return element;
     }
 
     changeVisibilityScreen(element, mode) {
         mode === VISIBLE_SCREEN ? element.classList.remove('HIDDEN_CLASS') : element.classList.add(HIDDEN_CLASS);
+        mode === VISIBLE_SCREEN ? element.classList.remove(HIDDEN_CLASS) : element.classList.add(HIDDEN_CLASS);
     }
 }
--- scripts/MainMenu.esm.js
+++ scripts/MainMenu.esm.js
@@ -1,4 +1,5 @@
 import { Common, HIDDEN_SCREEN, VISIBLE_SCREEN } from './Common.esm.js'
+import { levelSelect } from './LevelSelect.esm.js'
 
 const SCALE_PROPERTY = '--scale-value'
 const START_SCREEN_SETTINGS_BUTTON_ID = 'js-settings-button';
@@ -11,31 +12,29 @@ class MainMenu extends Common {
         this.resizeGameWindow();
         window.addEventListener('resize', this.resizeGameWindow);
     }
-
     bindToGameElements() {
         const gameStartButton = this.bindToElement(START_SCREEN_GAME_BUTTON_ID);
         const gameSettingsButton = this.bindToElement(START_SCREEN_SETTINGS_BUTTON_ID);
 
-        gameStartButton.addEventListener('click', () => this.showLevelScreen())
-        gameSettingsButton.addEventListener('click', () => this.showSettingsScreen())
         gameStartButton.addEventListener('click', this.showLevelScreen)
         gameSettingsButton.addEventListener('click', this.showSettingsScreen)
+        gameStartButton.addEventListener('click', () => this.showLevelScreen())
+        gameSettingsButton.addEventListener('click', () => this.showSettingsScreen())
     }
 
     showLevelScreen() {
         console.log('wybor poziomu')
+        this.changeVisibilityScreen(this.element, HIDDEN_SCREEN)
+        this.changeVisibilityScreen(levelSelect.element, VISIBLE_SCREEN)
     }
 
     showSettingsScreen() {
         console.log('wybor ustawien')
     }
-
     resizeGameWindow() {
         const { innerWidth: width, innerHeight: height } = window;
         const scale = Math.min(width / 640, height / 480);
-
         document.documentElement.style.setProperty(SCALE_PROPERTY, scale);
     }
 }
-
 export const mainMenu = new MainMenu();

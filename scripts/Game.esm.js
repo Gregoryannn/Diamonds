d9c3d7f830d255b1267302e4989fd7fc4ba5a6ea
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-02-26 08:59:24 +0000

    Add game level layer

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -11,11 +11,12 @@ class Game extends Common {
     playLevel(level) {
         window.removeEventListener(DATALOADED_EVENT_NAME, this.playLevel)
         const levelInfo = gameLevels[level - 1];
+        this.changeVisibilityScreen(canvas.element, VISIBLE_SCREEN);
         this.animate()
     }
 
     animate() {
-        console.log('game')
+        canvas.drawGameOnCanvas();
         this.animationFrame = window.requestAnimationFrame(() => this.animate());
     }
 }

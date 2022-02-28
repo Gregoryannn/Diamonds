7c2d4014ddb8d5735d1f02bb0886479f6ceda8fa
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-02-28 10:16:05 +0000

    Add Diamond and draw one diamond on a canvas

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -1,6 +1,12 @@
-import { Common } from './Common.esm.js';
+import { Common, VISIBLE_SCREEN } from './Common.esm.js';
 import { gameLevels } from './gameLevels.esm.js';
 import { loader, DATALOADED_EVENT_NAME } from './Loader.esm.js'
+import { Diamond } from './Diamond.esm.js';
+import { media } from './Media.esm.js';
+
+export const GAME_BOARD_X_OFFSET = 40;
+export const GAME_BOARD_Y_OFFSET = -5;
+
 
 
 const gameState = {
@@ -18,11 +24,13 @@ class Game extends Common {
         window.removeEventListener(DATALOADED_EVENT_NAME, this.playLevel)
         const levelInfo = gameLevels[level - 1];
         this.changeVisibilityScreen(canvas.element, VISIBLE_SCREEN);
+        this.diamond = new Diamond(50, 50, 1, 1, 2, media.diamondsSprite)
         this.animate()
     }
 
     animate() {
         canvas.drawGameOnCanvas(gameState);
+        this.diamond.draw()
         this.animationFrame = window.requestAnimationFrame(() => this.animate());
     }
 }

0126a503577aa5bbb714a9a6ebc0c462902098e6
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-03-03 11:53:06 +0000

    Add MouseControler

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -1,16 +1,15 @@
 import { Common, VISIBLE_SCREEN } from './Common.esm.js';
-import { gameLevels } from './gameLevels.esm.js';
-import { loader, DATALOADED_EVENT_NAME } from './Loader.esm.js'
-import { Diamond } from './Diamond.esm.js';
+import { canvas } from './Canvas.esm.js';
+import { gameLevels, GAME_BOARD_X_OFFSET } from './gameLevels.esm.js';
+import { DATALOADED_EVENT_NAME } from './Loader.esm.js'
 import { media } from './Media.esm.js';
 import { GameState } from './GameState.esm.js';
+import { mouseControler } from './MouseControler.esm.js';
+import { DIAMOND_SIZE } from './Diamond.esm.js';
 
 
-const gameState = {
-    pointsToWin: 7000,
-    getPlayerPoints: () => 1000,
-    getLeftMovement: () => 10,
-}
+const DIAMONDS_ARRAY_WIDTH = 8;
+const DIAMONDS_ARRAY_HEIGHT = DIAMONDS_ARRAY_WIDTH + 1; // first line is invisible
 
 class Game extends Common {
     constructor() {
@@ -27,10 +26,67 @@ class Game extends Common {
     }
 
     animate() {
+        this.handleMouseState();
+        this.handleMouseClick();
         canvas.drawGameOnCanvas(gameState);
         this.gameState.getGameBoard().forEach(diamond => diamond.draw());
         this.animationFrame = window.requestAnimationFrame(() => this.animate());
     }
+
+    handleMouseState() {
+        const isSwaping = !this.gameState.getIsSwaping();
+        const isMoving = !this.gameState.getIsMoving();
+        if (mouseControler.clicked && isSwaping && isMoving) {
+            mouseControler.state++;
+        }
+    }
+
+    handleMouseClick() {
+        if (!mouseControler.clicked) {
+            return;
+        }
+
+        const xClicked = Math.floor((mouseControler.x - GAME_BOARD_X_OFFSET) / DIAMOND_SIZE);
+        const yClicked = Math.floor((mouseControler.y - GAME_BOARD_Y_OFFSET) / DIAMOND_SIZE);
+
+        if (!yClicked || xClicked >= DIAMONDS_ARRAY_WIDTH || yClicked >= DIAMONDS_ARRAY_HEIGHT) {
+            mouseControler.state = 0;
+
+            return;
+        }
+
+        if (mouseControler.state === 1) {
+            mouseControler.firstClick = {
+                x: xClicked,
+                y: yClicked
+            }
+        } else if (mouseControler.state === 2) {
+            mouseControler.secondClick = {
+                x: xClicked,
+                y: yClicked
+            }
+
+            mouseControler.state = 0;
+
+            if (
+                Math.abs(mouseControler.firstClick.x - mouseControler.secondClick.x) +
+                Math.abs(mouseControler.firstClick.y - mouseControler.secondClick.y) !==
+                1
+            ) {
+                return;
+            }
+
+            this.swapDiamonds();
+
+            this.gameState.setIsSwaping(true);
+            this.gameState.decreasePointsMovement();
+            mouseControler.state = 0;
+        }
+
+        mouseControler.clicked = false;
+    }
 }
+
+
 
 export const game = new Game();

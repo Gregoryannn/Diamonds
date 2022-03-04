cb37c2cf0ee22dc59f73392a24ea0473609c4894
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-03-04 12:02:01 +0000

    Add findMatches method

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -1,6 +1,6 @@
 import { Common, VISIBLE_SCREEN } from './Common.esm.js';
 import { canvas } from './Canvas.esm.js';
-import { gameLevels, GAME_BOARD_X_OFFSET, GAME_BOARD_Y_OFFSET } from './gameLevels.esm.js';
+import { EMPTY_BLOCK, gameLevels, GAME_BOARD_X_OFFSET, GAME_BOARD_Y_OFFSET } from './gameLevels.esm.js';
 import { DATALOADED_EVENT_NAME } from './Loader.esm.js'
 import { media } from './Media.esm.js';
 import { GameState } from './GameState.esm.js';
@@ -10,6 +10,7 @@ import { DIAMOND_SIZE } from './Diamond.esm.js';
 
 const DIAMONDS_ARRAY_WIDTH = 8;
 const DIAMONDS_ARRAY_HEIGHT = DIAMONDS_ARRAY_WIDTH + 1; // first line is invisible
+const LAST_ELEMENT_DIAMONDS_ARRAY = DIAMONDS_ARRAY_WIDTH * DIAMONDS_ARRAY_HEIGHT - 1;
 const SWAPING_SPEED = 8;
 
 
@@ -30,6 +31,7 @@ class Game extends Common {
     animate() {
         this.handleMouseState();
         this.handleMouseClick();
+        this.findMatches();
         this.moveDiamonds();
         this.revertSwap();
         canvas.drawGameOnCanvas(this.gameState);
@@ -89,6 +91,42 @@ class Game extends Common {
 
         mouseControler.clicked = false;
     }
+
+
+
+    findMatches() {
+        this.gameState.getGameBoard().forEach((diamond, index, diamonds) => {
+            if (diamond.kind === EMPTY_BLOCK || index < DIAMONDS_ARRAY_WIDTH || index === LAST_ELEMENT_DIAMONDS_ARRAY) {
+                return;
+            }
+
+            if (
+                diamonds[index - 1].kind === diamond.kind
+                && diamonds[index + 1].kind === diamond.kind
+            ) {
+                if (Math.floor((index - 1) / DIAMONDS_ARRAY_WIDTH) === Math.floor(index + 1 / DIAMONDS_ARRAY_WIDTH)) {
+                    for (let i = -1; i <= 1; i++) {
+                        diamonds[index + i].match++;
+                    }
+                }
+            }
+
+            if (
+                index >= DIAMONDS_ARRAY_WIDTH
+                && index < LAST_ELEMENT_DIAMONDS_ARRAY - DIAMONDS_ARRAY_WIDTH + 1
+                && diamonds[index - DIAMONDS_ARRAY_WIDTH].kind === diamond.kind
+                && diamonds[index + DIAMONDS_ARRAY_WIDTH].kind === diamond.kind
+            ) {
+                if ((index - DIAMONDS_ARRAY_WIDTH) % DIAMONDS_ARRAY_WIDTH === (index + DIAMONDS_ARRAY_WIDTH) % DIAMONDS_ARRAY_WIDTH) {
+                    for (let i = -DIAMONDS_ARRAY_WIDTH; i <= DIAMONDS_ARRAY_WIDTH; i += DIAMONDS_ARRAY_WIDTH) {
+                        diamonds[index + i].match++
+                    }
+                }
+            }
+        });
+    }
+
+
 
     swapDiamonds() {
         const firstDiamond = mouseControler.firstClick.y * DIAMONDS_ARRAY_WIDTH + mouseControler.firstClick.x;
